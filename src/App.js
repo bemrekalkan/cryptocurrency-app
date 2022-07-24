884c329c7f2c47f4457b9929f85d0f5eab568ecd
bemrekalkan <bemre.kalkan@gmail.com>
2022-07-24 23:01:54 +0300

    structure going on

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@
 import React from "react";
 import { Switch, Route, Link } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
+import { Navbar } from "./components";
 
 const App = () => {
   return (
     <div className="app">
       <div className="navbar">
-        <div className="main">
-          <div className="footer"></div>
-        </div>
+        <Navbar />
       </div>
+      <div className="footer"></div>
+      <div className="main"></div>
     </div>
   );
 };
